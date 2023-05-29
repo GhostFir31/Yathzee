5052ea43d4369bb966e7524161f2181dd348e7a2
GhostFir31 <j3sus0marle@gmail.com>
2023-05-29 00:34:25 -0700

    Initial commit

---
+++ src/main/java/org/example/Jugador.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class Jugador {
+    ArrayList <Dado> cubilete;
+    public Jugador() {
+
+        int puntuaje;
+        cubilete = new ArrayList<Dado>();
+
+    }
+
+
+}
